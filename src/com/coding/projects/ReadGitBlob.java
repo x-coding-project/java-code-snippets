74e3cf13dbb9cf7f91e1f4724314dc2037fb26e6
bobbylin <1945775-bob-sgdev@users.noreply.gitlab.com>
2022-06-20 09:40:31 +0800

    Update Decompress method

--- src/com/coding/projects/ReadGitBlob.java
+++ src/com/coding/projects/ReadGitBlob.java
@@ -5,17 +5,17 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.zip.DataFormatException;
-import java.util.zip.Deflater;
 import java.util.zip.Inflater;
 
 public class ReadGitBlob {
 
     public static void main(String[] args) throws IOException, DataFormatException {
         String filePath ="src/test/resources/7acd32c4e687021ef32db511e8a206129b88ec"; // Read from test resources
-        decompressFile(filePath);
+        String content = decompressFile(filePath);
+        System.out.println(content);
     }
 
-    public static void decompressFile(String path) throws DataFormatException, IOException {
+    public static String decompressFile(String path) throws DataFormatException, IOException {
         byte[] fileBytes = new byte[0];
 
         fileBytes = Files.readAllBytes(Paths.get(path));
@@ -34,8 +34,7 @@ public class ReadGitBlob {
 
         // Decode the bytes into a String
         byte[] result = outputStream.toByteArray();
-        String outputString = new String(result, 0, result.length, StandardCharsets.UTF_8);
-        System.out.println(outputString);
 
+        return new String(result, 0, result.length, StandardCharsets.UTF_8);
     }
 }
